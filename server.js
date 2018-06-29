cbf6373e7555359b8b48c025ede4c6940b81d809
brendankim93 <31469167+brendankim93@users.noreply.github.com>
2018-06-28 19:55:19 -0700

    port fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-var PORT = 3900;
+var PORT = process.env.PORT || 3900;
 
 app.use("/assets", express.static(__dirname + '/assets'));
 
