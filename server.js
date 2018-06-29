4d0cd6a6682324e635a6e76fb3bff8714a083e32
brendankim93 <31469167+brendankim93@users.noreply.github.com>
2018-06-28 19:41:17 -0700

    express server added

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = 3900;
+
+app.use("/assets", express.static(__dirname + '/assets'));
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, function() {
+    console.log("App is listening on PORT: " + PORT);
+});
